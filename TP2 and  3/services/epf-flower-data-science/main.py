7c726cbb556655bc444757d358f5816f1aaad557
oscar92i <oscar.migeon@epfedu.fr>
2025-01-10 23:03:46 +0100

    step 3 4 5

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -1,8 +1,9 @@
 import uvicorn
-
 from src.app import get_application
 
+# Get the FastAPI application instance
 app = get_application()
 
+# Run the app with uvicorn, set to reload during development for auto-refresh
 if __name__ == "__main__":
     uvicorn.run("main:app", debug=True, reload=True, port=8080)
